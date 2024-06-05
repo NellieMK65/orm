2113568332d3e8c789e2e9c5b278525f3f1768dc
Nelson Muriithi <nelsonmuriithi5@gmail.com>
2024-06-05 15:43:13 +0300

    selecting & converting rows to class instance

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 -> a class instance can be associated with a table row
 """
 
+import datetime
 import sqlite3
 
 # create a connection to the db
@@ -41,7 +42,7 @@ class Menu:
 
     # return a printable representation of the object
     def __repr__(self) -> str:
-        return f"<Menu {self.id}: {self.name}, {self.description}>"
+        return f"<Menu {self.id}: {self.name}, {self.description}, {self.price}, {self.quantity}, {self.preparation_time}>"
 
     def save(self):
         """
@@ -80,6 +81,50 @@ class Menu:
 
         print(f"{self.name} with id {self.id} deleted")
 
+    def soft_delete(self):
+        sql = f"""
+            UPDATE {self.TABLE_NAME}
+            SET deleted_at = ?
+            WHERE id = ?
+        """
+        cursor.execute(sql, (datetime.datetime.now(), self.id))
+        conn.commit()
+
+        print(f"{self.name} with id {self.id} soft deleted")
+
+    @classmethod
+    def find_one(cls, id):
+        sql = f"""
+            SELECT * FROM {cls.TABLE_NAME}
+            WHERE id = ?
+        """
+        row = cursor.execute(sql, (id,)).fetchone()
+
+        if row == None:
+            return None
+
+        return cls.row_to_instance(row)
+
+    @classmethod
+    def find_all(cls, deleted = False):
+        where_clause = "IS NOT NULL" if deleted else "IS NULL"
+        sql = f"""
+            SELECT * FROM {cls.TABLE_NAME}
+            WHERE deleted_at {where_clause}
+        """
+
+        rows = cursor.execute(sql).fetchall()
+
+        return [cls.row_to_instance(row) for row in rows]
+
+    @classmethod
+    def row_to_instance(cls, row):
+        menu = cls(row[1], row[2], row[3], row[4], row[5])
+        menu.id = row[0]
+
+        return menu
+
+
     @classmethod
     def create_table(cls):
         cursor.execute(create_menus_table_sql)
@@ -94,14 +139,14 @@ class Menu:
 
     @classmethod
     def alter_table(cls, type, column_name, data_type = None):
-        sql = f"ALTER TABLE {cls.TABLE_NAME} DROP COLUMN {column_name}" if type == "drop" else f"ALTER TABLE {cls.TABLE_NAME} ADD COLUMN {column_name}"
+        sql = f"ALTER TABLE {cls.TABLE_NAME} DROP COLUMN {column_name}" if type == "drop" else f"ALTER TABLE {cls.TABLE_NAME} ADD COLUMN {column_name} {data_type}"
 
         cursor.execute(sql)
         conn.commit()
 
 # Menu.drop_table()
 Menu.create_table()
-# Menu.alter_table("drop", "preparation_time")
+# Menu.alter_table("", "deleted_at", "DATETIME")
 
 # rnb = Menu("Rice n Beans", "With Avocado", 120, 2, "40 mins")
 # rnb.save()
@@ -118,3 +163,35 @@ Menu.create_table()
 # rice_n_stew.update()
 
 # print(rice_n_stew)
+# menu_1 = Menu.find_one(2)
+
+# print(menu_1)
+
+# # update quantity
+# menu_1.quantity = menu_1.quantity - 60
+
+# menu_1.update()
+
+# print(menu_1)
+
+menus = Menu.find_all(deleted=True)
+print(menus)
+# menu_2 = menus[1]
+
+# menu_2.name = "Chapo n beans"
+# menu_2.price = 100
+# menu_2.quantity = 80
+# menu_2.preparation_time = "20 mins"
+
+# menu_2.delete()
+
+# menu_2.update()
+# menu_1.name = "Fries"
+
+# menu_1.update()
+
+# print(menu_1)
+
+# rnb = Menu.find_one(3)
+
+# rnb.soft_delete()
